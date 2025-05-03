ea1f5e6d1ba18945a08d6b5f36bd3eccc28b8a98
sunggyun-park <letsgopsg@naver.com>
2025-05-03 11:37:18 +0900

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.write('color:green; 안녕 성균')
+st.write('## 안녕 성균')
+
+
+
+
+
+
+
+
+
+
+
+import streamlit as st
+
+import streamlit as st
+
+st.markdown("<h2 style='color:green;'>안녕 성균</h2>", unsafe_allow_html=True)
+
