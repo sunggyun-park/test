f297777ca97717169a5541ab34b0b48d63120c35
sunggyun-park <letsgopsg@naver.com>
2025-05-03 12:10:25 +0900

    Add files via upload

--- aa.py
+++ aa.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 st.write('color:green; 안녕 성균')
-st.write('## 안녕 성균')
+st.write('## 	😄안녕 성균')
 
+col1, col2 = st.columns(2)
+with col1:
+   '하이 성균'
+with col2:
+   '##### 헬로우 성균'
 
-
-
-
-
-
-
+st.image("111.png", caption="성균이의 사진", use_column_width=True)
 
 
 
